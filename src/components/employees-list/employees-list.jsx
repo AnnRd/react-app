59e7eff807f87770171643838fe7af5df9eb4537
Anna Radchenko <anrd.rs>
2022-06-06 22:49:38 +0300

    dynamic emloyees-list

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -2,12 +2,14 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+const elements = data.map(item => {
+    return <EmployeesListItem name = {item.name} salary={item.salary}/>
+})
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem name ="John Brown" salary={800}/>
-            <EmployeesListItem name ="David Smith" salary={3000}/>
-            <EmployeesListItem name ="Michael Cornel" salary={5000}/>
+            {elements}
         </ul>
     );
 }
