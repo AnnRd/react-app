7d60e86499dc5600bdc786b0f399adb248957632
Anna Radchenko <anrd.rs>
2022-06-07 21:36:07 +0300

    states setted

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,9 +8,9 @@ import './app.css';
 
 function App() {
     const data = [
-        {name: 'John Brown', salary: 800, increase: false},
-        {name: 'David Smith', salary: 3000, increase: true},
-        {name: 'Michael Cornel', salary: 5000, increase: false}
+        {name: 'John Brown', salary: 800, increase: false, id: 1},
+        {name: 'David Smith', salary: 3000, increase: true, id: 2},
+        {name: 'Michael Cornel', salary: 5000, increase: false, id: 3}
     ];
 
     return (
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -4,7 +4,7 @@ import './employees-list.css';
 
 const EmployeesList = ({data}) => {
 const elements = data.map(item => {
-    return <EmployeesListItem name = {item.name} salary={item.salary} increase={item.increase}/> //можно использовать spread оп-тор {...item}, он развернет объект на отд. name и salary
+    return <EmployeesListItem name = {item.name} salary={item.salary} increase={item.increase} key={item.id}/> //можно использовать spread оп-тор {...item}, он развернет объект на отд. name и salary
 })
 
     return (
