b88c1fbf4ce60a4ac35abd41761de65254a00ca5
Anna Radchenko <anrd.rs>
2022-06-08 15:42:38 +0300

    add-form updated

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -18,6 +18,7 @@ class App extends Component {
                 {name: 'Michael Cornel', salary: 5000, increase: false, id: 3}
             ]
         }
+        this.maxId = 4;
     }
 
     deleteItem = (id) => {
@@ -25,6 +26,23 @@ class App extends Component {
             data: data.filter(item => item.id !== id)
         }));
         
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+
+        this.setState( ({data}) => {
+            const newArr = [...data, newItem];
+
+            return {
+                data: newArr
+            }
+        })
     }
 
     render() {
@@ -37,7 +55,7 @@ class App extends Component {
                     <AppFilter/>
                 </div>
                 <EmployeesList data = {this.state.data} onDelete = {this.deleteItem}/>
-                <EmployeesAddForm/>
+                <EmployeesAddForm onAdd = {this.addItem}/>
             </div>
         );
     }
--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -17,6 +17,16 @@ constructor(props) {
         })
     }
 
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
     render() {
         const {name, salary} = this.state;
 
@@ -24,7 +34,8 @@ constructor(props) {
             <div className="app-add-form">
                 <h3>Добавить сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Имя сотрудника"
